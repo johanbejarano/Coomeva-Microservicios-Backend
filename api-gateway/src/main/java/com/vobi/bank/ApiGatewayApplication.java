3d9631cc35601bb2af05d9aef767ab986e493201
Johan Steve Bejarano Fiesco <jbejarano@vortexbird.com>
2022-10-25 10:29:27 -0500

    Prueba de cambio en el nuevo repositorio de Coomeva

--- api-gateway/src/main/java/com/vobi/bank/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/vobi/bank/ApiGatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+	//Cambio
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
