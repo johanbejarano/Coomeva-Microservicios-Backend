881e207f18eb34a7c3341226cd8f3520c109e63c
Johan Steve Bejarano Fiesco <jbejarano@vortexbird.com>
2022-10-25 11:39:53 -0500

    Cambio en nombramiento de paquetes

---
+++ api-gateway/src/main/java/co/com/coomeva/bank/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package co.com.coomeva.bank.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	//Cambio
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
