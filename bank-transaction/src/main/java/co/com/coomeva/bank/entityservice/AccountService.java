51f5e7a9fcdd2bac7ff9eddf568c70c0a50ba63a
Johan Steve Bejarano Fiesco <jbejarano@vortexbird.com>
2022-10-26 10:18:42 -0500

    Cambio en nombramiento de paquetes

---
+++ bank-transaction/src/main/java/co/com/coomeva/bank/entityservice/AccountService.java
@@ -0,0 +1,11 @@
+package co.com.coomeva.bank.entityservice;
+
+import co.com.coomeva.bank.domain.Account;
+
+/**
+ * @author Zathura Code Generator Version 9.0 http://zathuracode.org/
+ *         www.zathuracode.org
+ *
+ */
+public interface AccountService extends GenericService<Account, String> {
+}
