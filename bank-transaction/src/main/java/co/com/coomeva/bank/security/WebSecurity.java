90200a22b326089e316e1068b573b32bba90d225
Johan Steve Bejarano Fiesco <jbejarano@vortexbird.com>
2023-06-01 17:03:45 -0500

    Migración a Springboot 3

--- bank-transaction/src/main/java/co/com/coomeva/bank/security/WebSecurity.java
+++ bank-transaction/src/main/java/co/com/coomeva/bank/security/WebSecurity.java
@@ -2,62 +2,96 @@ package co.com.coomeva.bank.security;
 
 import static co.com.coomeva.bank.security.Constants.ACTUATOR_URL;
 import static co.com.coomeva.bank.security.Constants.LOGIN_URL;
+import static co.com.coomeva.bank.security.Constants.API_DOCS;
+import static co.com.coomeva.bank.security.Constants.SWAGGER_UI;
 
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
 /**
- * @author Zathura Code Generator Version 9.0 http://zathuracode.org/
- *         www.zathuracode.org
- *
- */
+* @author Zathura Code Generator Version 23.03 http://zathuracode.org/
+* www.zathuracode.org
+* @generationDate 2023-05-03T09:19:19.667315
+* 
+*/
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurity {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
 
 	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
+	@Bean
+	SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+	
+		// Configure AuthenticationManagerBuilder
+        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+       
+        // Get AuthenticationManager
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+		
 		/*
-		 * 1. Se desactiva el uso de cookies 2. Se activa la configuraciï¿½n CORS con
-		 * los valores por defecto 3. Se desactiva el filtro CSRF 4. Se indica que el
-		 * login no requiere autenticaciï¿½n 5. Se indica que el resto de URLs esten
-		 * securizadas
+		 * 1. Se desactiva el uso de cookies
+		 * 2. Se activa la configuracion CORS con los valores por defecto
+		 * 3. Se desactiva el filtro CSRF
+		 * 4. Se indica que el login no requiere autenticacion
+		 * 5. Se indica que el resto de URLs esten securizadas
 		 */
-		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
-				.csrf().disable().authorizeRequests().antMatchers(LOGIN_URL, ACTUATOR_URL).permitAll().anyRequest()
-				.authenticated().and().addFilter(new JWTAuthenticationFilter(authenticationManager()))
-				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
+		
+
+
+        httpSecurity
+			.sessionManagement(sessionManager -> {
+				sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+			})
+			.authorizeHttpRequests( auth -> {
+				auth.requestMatchers(LOGIN_URL, ACTUATOR_URL, API_DOCS, SWAGGER_UI).permitAll();
+				auth.anyRequest().authenticated();
+			})
+			
+			.csrf( AbstractHttpConfigurer::disable )
+			.addFilter(new JWTAuthenticationFilter(authenticationManager))
+			.addFilter(new JWTAuthorizationFilter(authenticationManager))
+			.authenticationManager(authenticationManager)
+			.headers( headersConfig -> {
+				headersConfig.frameOptions().disable();
+			});
+	
+		 return httpSecurity.build(); 	
 	}
 
-	@Override
+
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// Se define la clase que recupera los usuarios y el algoritmo para procesar los
-		// passwords
+		// Se define la clase que recupera los usuarios y el algoritmo para procesar los passwords
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
-
+	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
@@ -70,4 +104,3 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	}
 
 }
-
